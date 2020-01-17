36c97af6599768cd66a82836aeb7d002d8da70ac
rlarlvy153 <rlarlvy153@naver.com>
2020-01-17 23:15:44 +0900

    rename register to signup

--- app/src/main/java/com/rollyglobe/rollyglobe/SignUpActivity.kt
+++ app/src/main/java/com/rollyglobe/rollyglobe/SignUpActivity.kt
@@ -7,8 +7,7 @@ import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import com.rollyglobe.rollyglobe.response_model.NationCodeModel
-import io.reactivex.disposables.CompositeDisposable
-import kotlinx.android.synthetic.main.activity_signin.*
+import kotlinx.android.synthetic.main.activity_signup.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -16,7 +15,7 @@ import retrofit2.Retrofit
 import java.util.*
 import kotlin.collections.ArrayList
 
-class SigninActivity : AppCompatActivity() {
+class SignUpActivity : AppCompatActivity() {
 //    lateinit var compositeDisposable: CompositeDisposable
     val TAG = "LoginActivity_kgp"
 
@@ -28,7 +27,7 @@ class SigninActivity : AppCompatActivity() {
     lateinit var retrofit: Retrofit
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signin)
+        setContentView(R.layout.activity_signup)
 
         val restClient:RollyGlobeApiInterface  =
             RetrofitCreator.getRetrofitService(RollyGlobeApiInterface::class.java)
@@ -58,7 +57,7 @@ class SigninActivity : AppCompatActivity() {
     fun initView(){
 
         //nation code spinner
-        val nationCodeSpinnerAdapter = ArrayAdapter(this@SigninActivity, android.R.layout.simple_spinner_item,nationCodeStringList)
+        val nationCodeSpinnerAdapter = ArrayAdapter(this@SignUpActivity, android.R.layout.simple_spinner_item,nationCodeStringList)
         nationCodeSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         nation_code_spinner.adapter = nationCodeSpinnerAdapter
 
@@ -78,7 +77,7 @@ class SigninActivity : AppCompatActivity() {
             yearList.add(y)
 
         }
-        val yearSpinnerAdapter = ArrayAdapter(this@SigninActivity, android.R.layout.simple_spinner_item,yearList)
+        val yearSpinnerAdapter = ArrayAdapter(this@SignUpActivity, android.R.layout.simple_spinner_item,yearList)
         yearSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         year_spinner.adapter = yearSpinnerAdapter
         year_spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
@@ -92,13 +91,13 @@ class SigninActivity : AppCompatActivity() {
         }
 
         //day spinner
-        dayAdapter = ArrayAdapter(this@SigninActivity, android.R.layout.simple_spinner_item, days)
+        dayAdapter = ArrayAdapter(this@SignUpActivity, android.R.layout.simple_spinner_item, days)
         dayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         day_spinner.adapter = dayAdapter
 
         //month spinner
         val monthList = listOf(1,2,3,4,5,6,7,8,9,10,11,12)
-        val monthSpinnerAdapter = ArrayAdapter(this@SigninActivity, android.R.layout.simple_spinner_item, monthList)
+        val monthSpinnerAdapter = ArrayAdapter(this@SignUpActivity, android.R.layout.simple_spinner_item, monthList)
         monthSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         month_spinner.adapter = monthSpinnerAdapter
 
@@ -141,15 +140,15 @@ class SigninActivity : AppCompatActivity() {
 
 
 
-        val dayAdapter = ArrayAdapter(this@SigninActivity, android.R.layout.simple_spinner_item, days)
+        val dayAdapter = ArrayAdapter(this@SignUpActivity, android.R.layout.simple_spinner_item, days)
         dayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         day_spinner.adapter = dayAdapter
     }
-    fun onClickRegister(v : View){
-        val temp_email = register_email_edit.text.toString()
-        val temp_nickname = register_nickname_edit.text.toString()
-        val temp_password = register_password.text.toString()
-        val temp_password_again = register_password_again.text.toString()
+    fun onClicksignup(v : View){
+        val temp_email = signup_email_edit.text.toString()
+        val temp_nickname = signup_nickname_edit.text.toString()
+        val temp_password = signup_password.text.toString()
+        val temp_password_again = signup_password_again.text.toString()
         val temp_gender_pos = gender_spinner.selectedItemPosition // 1남 2여
         val y = year_spinner.selectedItem.toString()
         val m = month_spinner.selectedItem.toString()
