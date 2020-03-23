4778decdd1b5b7212971f139dcc57df0dad1c079
rlarlvy153 <rlarlvy153@naver.com>
2020-03-23 23:43:47 +0900

    retain cookie & session

--- app/src/main/java/com/rollyglobe/rollyglobe/RetrofitCreator.kt
+++ app/src/main/java/com/rollyglobe/rollyglobe/RetrofitCreator.kt
@@ -1,5 +1,6 @@
 package com.rollyglobe.rollyglobe
 
+
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.Response
@@ -16,6 +17,7 @@ object RetrofitCreator {
 
     private var okHttpClient: OkHttpClient
     private var retrofit: Retrofit
+    val cookies = HashSet<String>()
 
     init{
         val httpLogging = HttpLoggingInterceptor()
@@ -25,6 +27,8 @@ object RetrofitCreator {
 
             addInterceptor(httpLogging)
             addInterceptor(HeaderSettingInterceptor())
+            addInterceptor(AddCookiesInterceptor())
+            addInterceptor(ReceivedCookiesInterceptor())
             connectTimeout(ALL_TIMEOUT, TimeUnit.SECONDS)
             writeTimeout(ALL_TIMEOUT, TimeUnit.SECONDS)
             readTimeout(ALL_TIMEOUT, TimeUnit.SECONDS)
@@ -48,13 +52,43 @@ object RetrofitCreator {
 
             val request = chainRequest.newBuilder().apply{
                 addHeader("Accept", "application/json")
+                removeHeader("User-Agent")
                 addHeader("User-Agent", "android-retrofit")
+
 //                addHeader("appKey", SK_API_KEY)
             }.build()
 
             return chain.proceed(request)
         }
     }
+    private class AddCookiesInterceptor : Interceptor{
+        @Throws(IOException::class)
+        override fun intercept(chain: Interceptor.Chain): Response {
+            val builder = chain.request().newBuilder()
+
+            for(cookie in cookies){
+                builder.addHeader("Cookie",cookie)
+            }
+            return chain.proceed(builder.build())
+        }
+    }
+
+    private class ReceivedCookiesInterceptor  : Interceptor{
+        @Throws(IOException::class)
+        override fun intercept(chain: Interceptor.Chain): Response {
+            val builder = chain.request().newBuilder()
+            val originalResponse = chain.proceed(builder.build())
+
+            if(!originalResponse.headers("Set-Cookie").isEmpty()){
+                for(header in originalResponse.headers("Set-Cookie")){
+                    cookies.add(header)
+                }
+            }
+            return originalResponse
+        }
+    }
+
+
     internal fun <T> getRetrofitService(restClass: Class<T>): T {
         return retrofit.create(restClass)
     }
