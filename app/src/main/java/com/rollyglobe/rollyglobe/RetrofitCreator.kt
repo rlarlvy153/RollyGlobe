72c5d66d8f0d774b12faa80d75f4606d2253ee08
rlarlvy153 <rlarlvy153@naver.com>
2020-04-02 23:53:28 +0900

    done so far

--- app/src/main/java/com/rollyglobe/rollyglobe/RetrofitCreator.kt
+++ app/src/main/java/com/rollyglobe/rollyglobe/RetrofitCreator.kt
@@ -8,6 +8,7 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import timber.log.Timber
 import java.io.IOException
 import java.util.concurrent.TimeUnit
 
@@ -20,6 +21,10 @@ object RetrofitCreator {
     val cookies = HashSet<String>()
 
     init{
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+
         val httpLogging = HttpLoggingInterceptor()
         httpLogging.level = HttpLoggingInterceptor.Level.BODY
 
