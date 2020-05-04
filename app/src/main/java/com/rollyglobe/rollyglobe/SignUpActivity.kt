5e6cb51969846cc689f3ae9192fd07864da68f19
rlarlvy153 <rlarlvy153@naver.com>
2020-05-04 11:54:20 +0900

    nation code spinner done

--- app/src/main/java/com/rollyglobe/rollyglobe/SignUpActivity.kt
+++ app/src/main/java/com/rollyglobe/rollyglobe/SignUpActivity.kt
@@ -38,6 +38,9 @@ class SignUpActivity : AppCompatActivity() {
         setContentView(R.layout.activity_signup)
         supportActionBar?.hide()
 
+        gender_birth_container.setOnClickListener {
+            Timber.d("clicked")
+        }
 
         disposable.add(restClient.getNationCodeInfoList()
             .subscribeOn(Schedulers.io())
@@ -63,7 +66,7 @@ class SignUpActivity : AppCompatActivity() {
         //nation code spinner
         val nationCodeSpinnerAdapter = ArrayAdapter(
             this@SignUpActivity,
-            android.R.layout.simple_spinner_item,
+            R.layout.nation_code_spinner_item,
             nationCodeStringList
         )
         nationCodeSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
@@ -80,95 +83,95 @@ class SignUpActivity : AppCompatActivity() {
 
 
         //year spinner
-        val timeZone = TimeZone.getTimeZone("Asia/Seoul")
-        val gregorianCalendar = GregorianCalendar(timeZone)
-        val year = gregorianCalendar.get(GregorianCalendar.YEAR)
+//        val timeZone = TimeZone.getTimeZone("Asia/Seoul")
+//        val gregorianCalendar = GregorianCalendar(timeZone)
+//        val year = gregorianCalendar.get(GregorianCalendar.YEAR)
+//
+//        val yearList = ArrayList<Int>()
+//        for (y in year downTo year - 100) {
+//            yearList.add(y)
+//
+//        }
+//        val yearSpinnerAdapter =
+//            ArrayAdapter(this@SignUpActivity, android.R.layout.simple_spinner_item, yearList)
+//        yearSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+//        year_spinner.adapter = yearSpinnerAdapter
+//        year_spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+//            override fun onItemSelected(
+//                praent: AdapterView<*>?,
+//                v: View?,
+//                position: Int,
+//                id: Long
+//            ) {
+//                updateLastDay()
+//            }
+//
+//            override fun onNothingSelected(p0: AdapterView<*>?) {
+//                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+//            }
+//        }
+//
+//        //day spinner
+//        dayAdapter = ArrayAdapter(this@SignUpActivity, android.R.layout.simple_spinner_item, days)
+//        dayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+//        day_spinner.adapter = dayAdapter
+//
+//        //month spinner
+//        val monthList = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
+//        val monthSpinnerAdapter =
+//            ArrayAdapter(this@SignUpActivity, android.R.layout.simple_spinner_item, monthList)
+//        monthSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+//        month_spinner.adapter = monthSpinnerAdapter
+//
+//        month_spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+//            override fun onItemSelected(
+//                praent: AdapterView<*>?,
+//                v: View?,
+//                position: Int,
+//                id: Long
+//            ) {
+//                updateLastDay()
+//            }
+//
+//            override fun onNothingSelected(p0: AdapterView<*>?) {
+//                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+//            }
+//        }
 
-        val yearList = ArrayList<Int>()
-        for (y in year downTo year - 100) {
-            yearList.add(y)
-
-        }
-        val yearSpinnerAdapter =
-            ArrayAdapter(this@SignUpActivity, android.R.layout.simple_spinner_item, yearList)
-        yearSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        year_spinner.adapter = yearSpinnerAdapter
-        year_spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(
-                praent: AdapterView<*>?,
-                v: View?,
-                position: Int,
-                id: Long
-            ) {
-                updateLastDay()
-            }
-
-            override fun onNothingSelected(p0: AdapterView<*>?) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-            }
-        }
-
-        //day spinner
-        dayAdapter = ArrayAdapter(this@SignUpActivity, android.R.layout.simple_spinner_item, days)
-        dayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        day_spinner.adapter = dayAdapter
-
-        //month spinner
-        val monthList = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
-        val monthSpinnerAdapter =
-            ArrayAdapter(this@SignUpActivity, android.R.layout.simple_spinner_item, monthList)
-        monthSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-        month_spinner.adapter = monthSpinnerAdapter
-
-        month_spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(
-                praent: AdapterView<*>?,
-                v: View?,
-                position: Int,
-                id: Long
-            ) {
-                updateLastDay()
-            }
-
-            override fun onNothingSelected(p0: AdapterView<*>?) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-            }
-        }
-
-
-    }
-
-    fun updateLastDay() {
-
-        val year = year_spinner.selectedItem as Int
-        val month = month_spinner.selectedItem as Int
-
-        val lastDay = when (month) {
-            1 -> 31
-            2 -> if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) 29 else 28
-            3 -> 31
-            4 -> 30
-            5 -> 31
-            6 -> 30
-            7 -> 31
-            8 -> 31
-            9 -> 30
-            10 -> 31
-            11 -> 30
-            12 -> 31
-            else -> 0
-        }
-
-        while (days.size < lastDay) {
-            days.add(days.size + 1)
-        }
-        while (days.size > lastDay) {
-            days.removeAt(days.size - 1)
-        }
-
-        dayAdapter.notifyDataSetChanged()
 
     }
+
+//    fun updateLastDay() {
+//
+//        val year = year_spinner.selectedItem as Int
+//        val month = month_spinner.selectedItem as Int
+//
+//        val lastDay = when (month) {
+//            1 -> 31
+//            2 -> if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) 29 else 28
+//            3 -> 31
+//            4 -> 30
+//            5 -> 31
+//            6 -> 30
+//            7 -> 31
+//            8 -> 31
+//            9 -> 30
+//            10 -> 31
+//            11 -> 30
+//            12 -> 31
+//            else -> 0
+//        }
+//
+//        while (days.size < lastDay) {
+//            days.add(days.size + 1)
+//        }
+//        while (days.size > lastDay) {
+//            days.removeAt(days.size - 1)
+//        }
+//
+//        dayAdapter.notifyDataSetChanged()
+//
+//    }
 
     fun onClickGenderButton(v: View) {
         when (v.id) {
@@ -202,9 +205,12 @@ class SignUpActivity : AppCompatActivity() {
 //        val temp_gender_pos = gender_spinner.selectedItemPosition // 1남 2여
         val temp_gender_pos = genderIndex
         val temp_gender = if (temp_gender_pos == 1) "male" else " female"
-        val y = year_spinner.selectedItem.toString()
-        val m = month_spinner.selectedItem.toString()
-        val d = day_spinner.selectedItem.toString()
+//        val y = year_spinner.selectedItem.toString()
+        val y = 1234.toString()
+//        val m = month_spinner.selectedItem.toString()
+        val m = 12.toString()
+//        val d = day_spinner.selectedItem.toString()
+        val d = 22.toString()
         val temp_nation = nation_code_spinner.selectedItem.toString()
         val temp_trim_nation = temp_nation.substring(1, temp_nation.indexOf('('))
         val contact = phone_number_edit.text.toString()
