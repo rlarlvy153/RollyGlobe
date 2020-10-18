62d879089bc9968fe8cf21666cedb7c2c04508e6
rlarlvy153 <rlarlvy153@naver.com>
2020-10-18 21:20:54 +0900

    signup refactoring

--- app/src/main/java/com/rollyglobe/support/di/ViewModelModule.kt
+++ app/src/main/java/com/rollyglobe/support/di/ViewModelModule.kt
@@ -4,6 +4,7 @@ package com.rollyglobe.support.di
 import com.rollyglobe.ui.MainViewModel
 import com.rollyglobe.ui.recommend.inner_contents.InnerContentsViewModel
 import com.rollyglobe.ui.signin.SignInViewModel
+import com.rollyglobe.ui.signup.SignUpViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
@@ -11,4 +12,5 @@ val viewModelModule = module {
     viewModel { MainViewModel() }
     viewModel { InnerContentsViewModel() }
     viewModel { SignInViewModel() }
+    viewModel { SignUpViewModel() }
 }
--- app/src/main/java/com/rollyglobe/ui/signup/SignUpActivity.kt
+++ app/src/main/java/com/rollyglobe/ui/signup/SignUpActivity.kt
@@ -14,18 +14,13 @@ import android.text.style.StyleSpan
 import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.Button
+import androidx.annotation.ColorRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.content.res.ResourcesCompat
+import androidx.lifecycle.Observer
 import com.rollyglobe.R
-import com.rollyglobe.network.RollyGlobeApiClient
-import com.rollyglobe.network.model.user.signup.SignUpOption
-import com.rollyglobe.network.model.user.signup.SignUpRequest
-import com.rollyglobe.network.model.user.signup.SignUpRequestModel
-import com.rollyglobe.network.model.response_model.NationCodeResponseModel
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.schedulers.Schedulers
+import com.rollyglobe.support.Utils
 import kotlinx.android.synthetic.main.activity_signup.*
 import org.koin.android.ext.android.inject
 import timber.log.Timber
@@ -35,54 +30,51 @@ import kotlin.collections.ArrayList
 
 class SignUpActivity : AppCompatActivity() {
 
-    val nationCodeList = ArrayList<NationCodeResponseModel>()
-    val nationCodeStringList = ArrayList<String>()
+    var nationCodeStringList = ArrayList<String>()
 
-    private val disposable = CompositeDisposable()
     var genderIndex = 1
     var selectedYear = 1997
     var selectedMonth = 11
     var selectedDay = 27
 
-    val restClient: RollyGlobeApiClient by inject()
-
-
-    lateinit var dayAdapter: ArrayAdapter<Int>
-
+    val signUpViewModel: SignUpViewModel by inject()
 
     val focusListesner = object : View.OnFocusChangeListener {
-        override fun onFocusChange(v: View?, hasFocus: Boolean) {
-            setAllUnderlineToGray()
-            val blue = ContextCompat.getColor(
-                this@SignUpActivity,
-                R.color.rg_blue
-            )
-            when (v?.id) {
-                signup_email_edit.id -> {
-                    signup_email_edit_underline.setBackgroundColor(blue)
-                }
-                signup_nickname_edit.id -> {
-                    signup_nickname_edit_underline.setBackgroundColor(blue)
-                }
-                signup_password.id -> {
-                    signup_password_edit_underline.setBackgroundColor(blue)
-                }
-                signup_password_again.id -> {
-                    signup_password_again_edit_underline.setBackgroundColor(blue)
-                }
+        fun getNewColor(hasFocus:Boolean) : Int{
+            if(hasFocus){
+                return ContextCompat.getColor(
+                    this@SignUpActivity,
+                    R.color.rg_blue
+                )
+            }
+            else{
+                return ContextCompat.getColor(
+                    this@SignUpActivity,
+                    R.color.rg_gray
+                )
             }
         }
+        override fun onFocusChange(v: View?, hasFocus: Boolean) {
 
-        private fun setAllUnderlineToGray() {
-            val gray = ContextCompat.getColor(
-                this@SignUpActivity,
-                R.color.rg_gray
-            )
+            val newColor = getNewColor(hasFocus)
 
-            signup_email_edit_underline.setBackgroundColor(gray)
-            signup_nickname_edit_underline.setBackgroundColor(gray)
-            signup_password_edit_underline.setBackgroundColor(gray)
-            signup_password_again_edit_underline.setBackgroundColor(gray)
+            when (v?.id) {
+                signUpEmailEdit.id -> {
+                    signUpEmailEditUnderline.setBackgroundColor(newColor)
+                }
+                signUpNicknameEdit.id -> {
+                    signUpNicknameEditUnderline.setBackgroundColor(newColor)
+                }
+                signup_password.id -> {
+                    signup_password_edit_underline.setBackgroundColor(newColor)
+                }
+                signup_password_again.id -> {
+                    signup_password_again_edit_underline.setBackgroundColor(newColor)
+                }
+                signUpPhoneNumberEdit.id->{
+                    signUpPhoneNumberEditUnderline.setBackgroundColor(newColor)
+                }
+            }
         }
     }
 
@@ -91,11 +83,89 @@ class SignUpActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
         supportActionBar?.hide()
-        signup_email_edit.onFocusChangeListener = focusListesner
-        signup_nickname_edit.onFocusChangeListener = focusListesner
+
+        addUnderlineFocusListener()
+
+        initTermText()
+
+        genderSelectListener()
+
+        signup_date_text.text = getString(
+            R.string.signup_date_text_string,
+            selectedYear,
+            selectedMonth,
+            selectedDay
+        )
+
+        observeEvents()
+
+        signUpViewModel.callNationCode()
+
+    }
+
+    private fun observeEvents() {
+
+        signUpViewModel.nationCodeString.observe(this, Observer {
+            nationCodeStringList = it
+
+            initNationCodeSpinner()
+        })
+
+        signUpViewModel.signUpErrorMsg.observe(this, Observer {
+            if (it.isNotBlank() && it.isNotEmpty()) {
+                Utils.showToast(it)
+            }
+        })
+
+        signUpViewModel.signUpResult.observe(this, Observer {
+            if(it){
+                Utils.showToast(Utils.getString(R.string.signup_success_signup))
+
+                finish()
+            }
+        })
+    }
+
+    private fun genderSelectListener() {
+        gender_birth_container.setOnClickListener {
+
+            val dialog = DatePickerDialog(
+                this,
+                OnDateSetListener { datePicker, year, month, date ->
+                    val msg =
+                        String.format("%d 년 %d 월 %d 일", year, month + 1, date)
+                    Timber.d(msg)
+
+//                    signup_date_text.text="$selectedYear    / $selectedMonth    /  $selectedDay"
+                    selectedYear = year
+                    selectedMonth = month + 1
+                    selectedDay = date
+                    signup_date_text.text = getString(
+                        R.string.signup_date_text_string,
+                        selectedYear,
+                        selectedMonth,
+                        selectedDay
+                    )
+                },
+                selectedYear,
+                selectedMonth,
+                selectedDay
+            )
+
+            dialog.datePicker.maxDate = Date().getTime() //입력한 날짜 이후로 클릭 안되게 옵션
+            dialog.show()
+        }
+    }
+
+    private fun addUnderlineFocusListener() {
+        signUpEmailEdit.onFocusChangeListener = focusListesner
+        signUpNicknameEdit.onFocusChangeListener = focusListesner
         signup_password.onFocusChangeListener = focusListesner
         signup_password_again.onFocusChangeListener = focusListesner
+        signUpPhoneNumberEdit.onFocusChangeListener = focusListesner
+    }
 
+    private fun initTermText() {
         val boldFont = ResourcesCompat.getFont(
             this,
             R.font.nanum_square_b
@@ -149,65 +219,9 @@ class SignUpActivity : AppCompatActivity() {
         signup_terms2.setText(result)
         signup_terms2.movementMethod = LinkMovementMethod.getInstance()
         signup_terms2.highlightColor = Color.TRANSPARENT
-
-
-
-        gender_birth_container.setOnClickListener {
-
-            val dialog = DatePickerDialog(
-                this,
-                OnDateSetListener { datePicker, year, month, date ->
-                    val msg =
-                        String.format("%d 년 %d 월 %d 일", year, month + 1, date)
-                    Timber.d(msg)
-
-//                    signup_date_text.text="$selectedYear    / $selectedMonth    /  $selectedDay"
-                    selectedYear = year
-                    selectedMonth = month + 1
-                    selectedDay = date
-                    signup_date_text.text = getString(
-                        R.string.signup_date_text_string,
-                        selectedYear,
-                        selectedMonth,
-                        selectedDay
-                    )
-                },
-                selectedYear,
-                selectedMonth,
-                selectedDay
-            )
-
-            dialog.datePicker.maxDate = Date().getTime() //입력한 날짜 이후로 클릭 안되게 옵션
-            dialog.show()
-        }
-
-        signup_date_text.text = getString(
-            R.string.signup_date_text_string,
-            selectedYear,
-            selectedMonth,
-            selectedDay
-        )
-
-        disposable.add(restClient.getNationCodeInfoList()
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({ result ->
-
-                for (code in result) {
-                    nationCodeStringList.add(code.toString())
-                }
-
-                initView()
-            }, {
-                Timber.d("error")
-            }
-
-            )
-        )
-
     }
 
-    fun initView() {
+    private fun initNationCodeSpinner() {
 
         //nation code spinner
         val nationCodeSpinnerAdapter = ArrayAdapter(
@@ -217,107 +231,7 @@ class SignUpActivity : AppCompatActivity() {
         )
         nationCodeSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         nation_code_spinner.adapter = nationCodeSpinnerAdapter
-
-        //gender spinner
-//        val genderSpinnerAdapter = ArrayAdapter.createFromResource(
-//            this,
-//            R.array.gender,
-//            android.R.layout.simple_spinner_item
-//        )
-//        genderSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-//        gender_spinner.adapter = genderSpinnerAdapter
-
-
-        //year spinner
-//        val timeZone = TimeZone.getTimeZone("Asia/Seoul")
-//        val gregorianCalendar = GregorianCalendar(timeZone)
-//        val year = gregorianCalendar.get(GregorianCalendar.YEAR)
-//
-//        val yearList = ArrayList<Int>()
-//        for (y in year downTo year - 100) {
-//            yearList.add(y)
-//
-//        }
-//        val yearSpinnerAdapter =
-//            ArrayAdapter(this@SignUpActivity, android.R.layout.simple_spinner_item, yearList)
-//        yearSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-//        year_spinner.adapter = yearSpinnerAdapter
-//        year_spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-//            override fun onItemSelected(
-//                praent: AdapterView<*>?,
-//                v: View?,
-//                position: Int,
-//                id: Long
-//            ) {
-//                updateLastDay()
-//            }
-//
-//            override fun onNothingSelected(p0: AdapterView<*>?) {
-//                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-//            }
-//        }
-//
-//        //day spinner
-//        dayAdapter = ArrayAdapter(this@SignUpActivity, android.R.layout.simple_spinner_item, days)
-//        dayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-//        day_spinner.adapter = dayAdapter
-//
-//        //month spinner
-//        val monthList = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12)
-//        val monthSpinnerAdapter =
-//            ArrayAdapter(this@SignUpActivity, android.R.layout.simple_spinner_item, monthList)
-//        monthSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-//        month_spinner.adapter = monthSpinnerAdapter
-//
-//        month_spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-//            override fun onItemSelected(
-//                praent: AdapterView<*>?,
-//                v: View?,
-//                position: Int,
-//                id: Long
-//            ) {
-//                updateLastDay()
-//            }
-//
-//            override fun onNothingSelected(p0: AdapterView<*>?) {
-//                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-//            }
-//        }
-
-
     }
-
-//    fun updateLastDay() {
-//
-//        val year = year_spinner.selectedItem as Int
-//        val month = month_spinner.selectedItem as Int
-//
-//        val lastDay = when (month) {
-//            1 -> 31
-//            2 -> if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) 29 else 28
-//            3 -> 31
-//            4 -> 30
-//            5 -> 31
-//            6 -> 30
-//            7 -> 31
-//            8 -> 31
-//            9 -> 30
-//            10 -> 31
-//            11 -> 30
-//            12 -> 31
-//            else -> 0
-//        }
-//
-//        while (days.size < lastDay) {
-//            days.add(days.size + 1)
-//        }
-//        while (days.size > lastDay) {
-//            days.removeAt(days.size - 1)
-//        }
-//
-//        dayAdapter.notifyDataSetChanged()
-//
-//    }
 
     fun onClickGenderButton(v: View) {
         when (v.id) {
@@ -352,68 +266,47 @@ class SignUpActivity : AppCompatActivity() {
     }
 
     fun onClicksignup(v: View) {
-        val temp_email = signup_email_edit.text.toString()
-        val temp_nickname = signup_nickname_edit.text.toString()
-        val temp_password = signup_password.text.toString()
-        val temp_password_again = signup_password_again.text.toString()
-//        val temp_gender_pos = gender_spinner.selectedItemPosition // 1남 2여
-        val temp_gender_pos = genderIndex
-        val temp_gender = if (temp_gender_pos == 1) "male" else " female"
+        val email = signUpEmailEdit.text.toString()
+        val nickname = signUpNicknameEdit.text.toString()
+        val password = signup_password.text.toString()
+        val passwordAgain = signup_password_again.text.toString()
+        val gender = if (genderIndex == 1) "male" else " female"
         val y = selectedYear.toString()
         val m = selectedMonth.toString()
         val d = selectedDay.toString()
-        val temp_nation = nation_code_spinner.selectedItem.toString()
-        val temp_trim_nation = temp_nation.substring(1, temp_nation.indexOf('('))
-        val contact = phone_number_edit.text.toString()
+        val nation = nation_code_spinner.selectedItem.toString()
+        val trimmedNation = nation.substring(1, nation.indexOf('('))
+        val contact = signUpPhoneNumberEdit.text.toString()
 
-
-
-        if (temp_password != temp_password_again) {
-
-            Timber.d("비밀번호가 다름 $temp_password $temp_password_again")
+        if (email.isBlank() || email.isEmpty()) {
+            Utils.showToast(Utils.getString(R.string.signup_empty_email))
             return
         }
-        if (temp_gender_pos == 0) {
-            Timber.e("성별 선택 안됨")
+        if (nickname.isBlank() || nickname.isEmpty()) {
+            Utils.showToast(Utils.getString(R.string.signup_empty_nickname))
             return
         }
-        Timber.i("메일 : $temp_email")
-        Timber.i("별명 : $temp_nickname")
-        Timber.i("비번 : $temp_password $temp_password_again")
 
+        if (password.isBlank() || password.isEmpty()) {
+            Utils.showToast(Utils.getString(R.string.signup_empty_password))
+            return
+        }
+        if (password != passwordAgain) {
+            Utils.showToast(Utils.getString(R.string.signup_different_password))
+            return
+        }
+        if(contact.isBlank() || contact.isEmpty()){
+            Utils.showToast(Utils.getString(R.string.signup_empty_contact))
+            return
+        }
 
-        Timber.i("국가번호 : $temp_trim_nation")
-        Timber.i("$temp_gender $y $m $d")
-
-
-        val option = SignUpOption(
-            temp_email,
-            temp_nickname,
-            temp_password,
-            temp_trim_nation,
+        signUpViewModel.signUp(
+            email,
+            nickname,
+            password,
+            trimmedNation,
             contact,
-            temp_gender,
-            y,
-            if (m.toInt() < 10) "0$m" else m,
-            if (d.toInt() < 10) "0$d" else d
+            gender, y, m, d
         )
-
-        val signUpRequestModel = SignUpRequestModel()
-        signUpRequestModel.request = SignUpRequest("SignUp", option)
-
-        disposable.add(restClient.signUp(signUpRequestModel)
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({ result ->
-                Timber.i(result.msg)
-                Timber.i("${result.success}")
-                finish()
-            },
-                {
-
-                }
-            )
-        )
-
     }
 }
