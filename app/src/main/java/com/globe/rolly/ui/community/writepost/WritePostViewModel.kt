7a54237aaf03d360526bee4a1eaed8909d347e8e
rlarlvy153 <rlarlvy153@naver.com>
2020-12-06 21:12:47 +0900

    load pictcure

--- app/src/main/java/com/globe/rolly/ui/community/writepost/WritePostViewModel.kt
+++ app/src/main/java/com/globe/rolly/ui/community/writepost/WritePostViewModel.kt
@@ -5,6 +5,7 @@ import android.net.Uri
 import android.provider.MediaStore
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
+import io.reactivex.Observable
 
 class WritePostViewModel(val app: Application) : AndroidViewModel(app) {
 
@@ -13,6 +14,38 @@ class WritePostViewModel(val app: Application) : AndroidViewModel(app) {
     private val imageHashMap = HashMap<String, ArrayList<String>>()
 
     fun loadImagesByUri() {
+
+        imageHashMap["전체보기"] = ArrayList<String>()
+        val allImagesuri: Uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        val projection = arrayOf(
+            MediaStore.Images.ImageColumns.DATA, MediaStore.Images.Media.DISPLAY_NAME,
+            MediaStore.Images.Media.BUCKET_DISPLAY_NAME, MediaStore.Images.Media.BUCKET_ID
+        )
+        val cursor = app.contentResolver.query(allImagesuri, projection, null, null, null)
+
+        try {
+            cursor?.moveToFirst()
+            do {
+                val folder = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Images.Media.BUCKET_DISPLAY_NAME))
+                val datapath = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA))
+
+                if (imageHashMap[folder] == null) {
+                    imageHashMap[folder] = ArrayList<String>()
+                }
+                imageHashMap[folder]!!.add(datapath)
+                imageHashMap["전체보기"]!!.add(datapath)
+
+            } while (cursor.moveToNext())
+            cursor.close()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+        imageHashMapLiveData.value = imageHashMap["전체보기"]
+    }
+
+    fun loadImagesByUriObservable() {
+
 
         imageHashMap["전체보기"] = ArrayList<String>()
         val allImagesuri: Uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
